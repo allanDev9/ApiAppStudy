3954e30442d4f885d049cae1e2e507e156367553
allanDev9 <allanpenaguerra2003@gmail.com>
2026-03-20 17:51:04 -0600

    Elimine dokcerfile

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiAppStudy.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define tus DbSets (tablas)
+        public DbSet<Activity> Activites { get; set; }
+
+    }
+}
